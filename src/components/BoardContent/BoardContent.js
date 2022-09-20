ca692f37f4b520b1666c6a7b9bf4fdb4946c91fe
Haofphm4426 <haofphm4426@gmail.com>
2022-09-20 16:56:41 +0700

    sample data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,15 +1,39 @@
-import Column from 'components/Column/Column';
+import { useState, useEffect } from 'react';
+import { isEmpty } from 'lodash';
+
 import './BoardContent.scss';
 
+import Column from 'components/Column/Column';
+import { mapOrder } from 'utilities/sorts';
+
+import { initData } from 'actions/initData';
+
 function BoardContent() {
-    return (
-        <div className="board-content">
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-        </div>
-    );
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const boardFormDB = initData.boards.find((board) => board.id === 'board-1');
+    if (boardFormDB) {
+      setBoard(boardFormDB);
+
+      //sort column
+      mapOrder(boardFormDB.columns, boardFormDB.columnOrder, 'id');
+      setColumns(boardFormDB.columns);
+    }
+  }, []);
+
+  if (isEmpty(board)) {
+    return <div className="not-found">Board Not Found</div>;
+  }
+
+  return (
+    <div className="board-content">
+      {columns.map((column, idx) => (
+        <Column key={idx} column={column} />
+      ))}
+    </div>
+  );
 }
 
 export default BoardContent;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import './Card.scss';
+
+function Card({ card }) {
+  return (
+    <li className="card-item">
+      {card.cover && <img src={card.cover} className="card-cover" alt="" />}
+
+      <div className="title">{card.title}</div>
+    </li>
+  );
+}
+
+export default Card;
