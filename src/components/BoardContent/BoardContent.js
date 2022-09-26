45823911d50a12da43923c1596daa30b6a78b255
Haofphm4426 <haofphm4426@gmail.com>
2022-09-26 14:49:02 +0700

    add_new_column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,5 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import { Container, Draggable } from 'react-smooth-dnd';
+import { Container as BootstrapContainer, Row, Col, Form, Button } from 'react-bootstrap';
+
 import { isEmpty } from 'lodash';
 
 import './BoardContent.scss';
@@ -9,10 +11,18 @@ import { mapOrder } from 'utilities/sorts';
 import { applyDrag } from 'utilities/dragDrop';
 
 import { initData } from 'actions/initData';
+import { useRef } from 'react';
+import { useCallback } from 'react';
 
 function BoardContent() {
     const [board, setBoard] = useState({});
     const [columns, setColumns] = useState([]);
+    const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+
+    const newColumnInputRef = useRef(null);
+
+    const [newColumnTitle, setNewColumnTitle] = useState('');
+    const onNewColumnTitleChange = useCallback((e) => setNewColumnTitle(e.target.value), []);
 
     useEffect(() => {
         const boardFormDB = initData.boards.find((board) => board.id === 'board-1');
@@ -24,6 +34,13 @@ function BoardContent() {
             boardFormDB.columns = mapOrder(boardFormDB.columns, boardFormDB.columnOrder, 'id');
         }
     }, []);
+
+    useEffect(() => {
+        if (newColumnInputRef && newColumnInputRef.current) {
+            newColumnInputRef.current.focus();
+            newColumnInputRef.current.select();
+        }
+    }, [openNewColumnForm]);
 
     if (isEmpty(board)) {
         return <div className="not-found">Board Not Found</div>;
@@ -53,6 +70,36 @@ function BoardContent() {
         }
     };
 
+    const toggleOpenForm = () => {
+        setOpenNewColumnForm(!openNewColumnForm);
+    };
+
+    const addNewColumn = () => {
+        if (!newColumnTitle) {
+            newColumnInputRef.current.focus();
+            return;
+        }
+
+        const newColumnToAdd = {
+            id: Math.random().toString(36).substr(2, 5), // random characters
+            boardId: board.id,
+            title: newColumnTitle.trim(),
+            cardOrder: [],
+            cards: [],
+        };
+        let newColumns = [...columns];
+        newColumns.push(newColumnToAdd);
+
+        let newBoard = { ...board };
+        newBoard.columnOrder = newColumns.map((c) => c.id);
+        newBoard.columns - newColumns;
+
+        setColumns(newColumns);
+        setBoard(newBoard);
+        setNewColumnTitle('');
+        toggleOpenForm();
+    };
+
     return (
         <div className="board-content">
             <Container
@@ -72,10 +119,40 @@ function BoardContent() {
                     </Draggable>
                 ))}
             </Container>
-            <div className="add-new-column">
-                <i className="fa fa-plus icon" />
-                Add another column
-            </div>
+
+            <BootstrapContainer className="trello-container">
+                {!openNewColumnForm && (
+                    <Row>
+                        <Col className="add-new-column" onClick={toggleOpenForm}>
+                            <i className="fa fa-plus icon" />
+                            Add another column
+                        </Col>
+                    </Row>
+                )}
+
+                {openNewColumnForm && (
+                    <Row>
+                        <Col className="enter-new-column">
+                            <Form.Control
+                                size="sm"
+                                type="text"
+                                placeholder="Enter column title"
+                                className="input-enter-new-column"
+                                ref={newColumnInputRef}
+                                value={newColumnTitle}
+                                onChange={onNewColumnTitleChange}
+                                onKeyDown={(e) => e.key === 'Enter' && addNewColumn()}
+                            />
+                            <Button variant="success" size="sm" onClick={addNewColumn}>
+                                Add column
+                            </Button>
+                            <span className="cancel-new-column">
+                                <i className="fa fa-trash icon" onClick={toggleOpenForm}></i>
+                            </span>
+                        </Col>
+                    </Row>
+                )}
+            </BootstrapContainer>
         </div>
     );
 }
